8b8931e00d1682b0e68a028eb0bd844e872c62db
Crowdbotics <team@crowdbotics.com>
2021-09-11 01:00:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "rapid_tree_30454Identifier",
+  url: "https://rapid_tree_30454Identifier.botics.co",
+  api: "https://rapid_tree_30454Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
